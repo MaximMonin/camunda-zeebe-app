3e11e8cd6ee41e246a85a2a1bd96c559aba2fe47
Maxim Monin <maximmonin@gmail.com>
2020-10-17 12:25:09 +0000

    added pure websocket protocol to subscribe to camunda async external task callbacks from ws server to web client

--- app/javascript/camunda/client.js
+++ app/javascript/camunda/client.js
@@ -1,40 +1,55 @@
 const axios = require('axios');
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-import { createConsumer } from "@rails/actioncable"
-const { v4: uuidv4 } = require('uuid');
 
 class CamundaClient {
   constructor(url, wsurl) {
     this.url = url;
-    this.consumer = createConsumer ( wsurl );
+    this.wsurl = wsurl
+    this.socket = null;
+    this.socketstatus = 0;
     this.processId = null;
-    this.businessKey = null;
     this.processKey = null;
   };
   async startProcess(ProcessKey, Variables, callback) {
     this.processKey = ProcessKey;
-    this.BusinessKey = ProcessKey + "-" + uuidv4();
+    try {
+      var socket = new WebSocket("ws://195.39.196.61:2586/", ["Camunda"]);
+      this.socket = socket;
+      // Соединение открыто
+      socket.onopen = function (event) {
+        console.log ('ws connection opened');
+        this.socketstatus = 1;
+      };
+      socket.onclose = function(event) {
+        console.log ('ws connection closed');
+        this.socketstatus = 0;
+      };
+      socket.onerror = function (error) {
+        console.log('ws error ' + error);
+      };
+      socket.onmessage = function (event) {
+        if (event.data === undefined) {
+           return;
+        }
+        if (event.data == 'welcome') {
+          socket.send ("welcome");
+          return;
+        }
+        if (this.processId == event.data.processId) {
+          callback (event.data);
+        }
+      };
+    }
+    catch (e) {
+      console.log(e);
+    }
  
-    await axios.post( this.url + '/engine-rest/process-definition/key/' + ProcessKey + '/start', {variables: Variables, businessKey: this.BusinessKey})
+    await axios.post( this.url + '/engine-rest/process-definition/key/' + ProcessKey + '/start', {variables: Variables})
     .then(response => {
      const data = response.data;
      console.log('Workflow instance started: ' + JSON.stringify(data));
      this.processId = data.id;
-     this.processKey = ProcessKey;
-     this.businessKey = data.businessKey;
-     this.consumer.subscriptions.create({ channel: "Camunda", room: this.businessKey },
-     {
-       connected() {
-         console.log ('ws connection established');
-       },
-       disconnected() {
-         console.log ('ws connection closed');
-       },
-       received(data) {
-         callback(data);
-       },
-     });
-
+     this.socket.send (JSON.stringify({command: 'subscribe', channel: 'Camunda', processId: this.processId}));
      return data;
     })
     .catch(error => {
