cc9ce73f149c6ea60665e9811d42623468ea8188
Maxim Monin <maximmonin@gmail.com>
2020-10-17 21:43:55 +0000

    added publish message to cumunda client

--- app/javascript/camunda/client.js
+++ app/javascript/camunda/client.js
@@ -6,23 +6,66 @@ class CamundaClient {
     this.url = url;
     this.wsurl = wsurl
     this.socket = null;
-    this.socketstatus = 0;
     this.processId = null;
     this.processKey = null;
+    this.callback = null;
   };
   async startProcess(ProcessKey, Variables, callback) {
     this.processKey = ProcessKey;
+    this.OpenSocket (callback);
+ 
+    await axios.post( this.url + '/engine-rest/process-definition/key/' + ProcessKey + '/start', {variables: Variables})
+    .then(response => {
+     const data = response.data;
+     console.log('Workflow instance started: ' + JSON.stringify(data));
+     this.processId = data.id;
+     this.socket.send (JSON.stringify({command: 'subscribe', channel: 'Camunda', processId: this.processId}));
+     return data;
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.data);
+      }
+      return error;
+    });
+  };
+  async publishMessage (messageName, Variables, callback) {
+    this.OpenSocket (callback);
+    await axios.post( this.url + '/engine-rest/message', {messageName: messageName, processInstanceId: this.processId, processVariables: Variables})
+    .then(response => {
+     const data = response.data;
+     console.log('Message delivered: ' + JSON.stringify(data));
+     return data;
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.data);
+      }
+      return error;
+    });
+
+  }
+  OpenSocket (callback) {
+    this.callback = callback;
+    if (this.socket && this.socket.readyState == 1) {
+      return;
+    }
     try {
-      var socket = new WebSocket("ws://195.39.196.61:2586/", ["Camunda"]);
+      var wsUrl = '';
+      if (window.location.protocol == 'http:') {
+        wsUrl = 'ws://' + window.location.host + '/' + this.wsurl;
+      }
+      else {
+        wsUrl = 'wss://' + window.location.host + '/' + this.wsurl;
+      }
+      var socket = new WebSocket(wsUrl, ["Camunda"]);
       this.socket = socket;
       // Соединение открыто
       socket.onopen = function (event) {
         console.log ('ws connection opened');
-        this.socketstatus = 1;
       };
       socket.onclose = function(event) {
         console.log ('ws connection closed');
-        this.socketstatus = 0;
       };
       socket.onerror = function (error) {
         console.log('ws error ' + error);
@@ -43,22 +86,7 @@ class CamundaClient {
     catch (e) {
       console.log(e);
     }
- 
-    await axios.post( this.url + '/engine-rest/process-definition/key/' + ProcessKey + '/start', {variables: Variables})
-    .then(response => {
-     const data = response.data;
-     console.log('Workflow instance started: ' + JSON.stringify(data));
-     this.processId = data.id;
-     this.socket.send (JSON.stringify({command: 'subscribe', channel: 'Camunda', processId: this.processId}));
-     return data;
-    })
-    .catch(error => {
-      if (error.response) {
-        console.log(error.response.data);
-      }
-      return error;
-    });
-  };
+  }
 };
 
 export {
